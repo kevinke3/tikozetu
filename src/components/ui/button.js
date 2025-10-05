17b7d04dca3bf5551f3dec1ac4258e925023f79a
kevinke3 <kevinke386@gmail.com>
2025-10-05 18:55:16 +0300

    Updates

---
+++ src/components/ui/button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Button = ({ children, onClick, className = '', type = 'button' }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
